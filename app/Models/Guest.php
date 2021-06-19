b51a7f73de5f0f86481695a7555bf24c4dd732e1
nizarAB <nizaraitbrahim1@gmail.com>
2021-06-19 12:59:03 +0100

    Tadkirati-laravel

---
+++ app/Models/Guest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Guest extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'num_tel',
+        'email',
+        'voyages_id',
+    ];
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
